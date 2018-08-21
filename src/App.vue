c5bb48d3180aa0ac9d85c725514d1809bc004a0e
YangXia1 <1430594709@qq.com>
2018-08-21 17:10:02 +0800

    up

--- src/App.vue
+++ src/App.vue
@@ -19,13 +19,13 @@ export default {
 </script>
 
 <style>
-#app {
+#app,body {
   font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  /* margin-top: 60px; */
   background: #fff;
 }
 .svg-icon {
--- src/views/demo/detail/detail.js
+++ src/views/demo/detail/detail.js
@@ -5,20 +5,22 @@ export
 default {
     data() {
         return {
+            selectsName: [],
+            selectsId: [],
             foods: [{
                     id: 1,
                     classifiy: '猪肉',
                     list: [{
                             id: 2,
-                            name: '量少'
+                            name: '量少1'
                         },
                         {
                             id: 3,
-                            name: '量少'
+                            name: '不新鲜'
                         },
                         {
                             id: 4,
-                            name: '量少'
+                            name: '太油腻'
                         },
                     ]
                 },
@@ -37,29 +39,63 @@ default {
                             id: 9,
                             name: '太油'
                         },
+                        {
+                            id: 10,
+                            name: '量少'
+                        },
+                        {
+                            id: 11,
+                            name: '太油'
+                        },
                     ]
                 },
                 {
-                    id: 10,
+                    id: 12,
                     classifiy: '水果',
                     list: [{
-                            id: 11,
+                            id: 13,
                             name: '不新鲜'
                         },
                         {
-                            id: 12,
+                            id: 14,
                             name: '种类太少'
                         },
                         {
-                            id: 13,
+                            id: 15,
                             name: '过期'
+                        },
+                        {
+                            id: 16,
+                            name: '种类太少'
+                        },
+                        {
+                            id: 17,
+                            name: '过期'
+                        },
+                        {
+                            id: 18,
+                            name: '种类太少'
                         },
                     ]
                 },
             ]
         }
     },
-    methods: {
+    created() {
 
+    },
+    methods: {
+        /**
+         * 选择类目
+         */
+        selectFoods(id) {
+            let isHas = this.selectsName.findIndex(item => {
+                return item.id
+            });
+            // 如果该类目没有被选择，则选中
+            if (isHas === -1) {
+
+            }
+        }
     }
 }
